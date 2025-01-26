234a9e5bbfe754a5e48e8ebf35cca13d3f01aeac
Chetan Chaudhari <chetankc2005@gmail.com>
2025-01-26 16:29:14 +0530

    Navbar Added (Incomplete)

--- JS Files/app.js
+++ JS Files/app.js
@@ -9,3 +9,13 @@ sign_up_btn.addEventListener("click", () => {
 sign_in_btn.addEventListener("click", () => {
   main_container.classList.remove("sign-up-mode");
 });
+
+// Select the required elements
+const toggleButton = document.querySelector(".custom_menu-btn");
+const navMenu = document.querySelector("#navbarSupportedContent");
+
+// Toggle the menu visibility and button style
+toggleButton.addEventListener("click", () => {
+  navMenu.classList.toggle("lg_nav-toggle");
+  toggleButton.classList.toggle("menu_btn-style");
+});
