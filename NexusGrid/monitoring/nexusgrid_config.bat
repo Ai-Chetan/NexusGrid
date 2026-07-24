6df5315cc2170351dd0e2249b7cf2545f9dbe1df
ParthShikhare19 <parthshikhare21@gmail.com>
2026-07-24 12:06:11 +0530

    fix(monitoring): fix hanging installation and systemd persistence on Linux

--- NexusGrid/monitoring/nexusgrid_config.bat
+++ NexusGrid/monitoring/nexusgrid_config.bat
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-set "NEXUSGRID_BASE_URL=http://127.0.0.1:8000"
-=======
 set "NEXUSGRID_BASE_URL=https://nexusgrid.onrender.com"
->>>>>>> main
 set "PYTHON_EXE=C:\Users\admin\AppData\Local\Programs\Python\Python313\python.exe"
 set "PYTHONW_EXE=C:\Users\admin\AppData\Local\Programs\Python\Python313\pythonw.exe"
